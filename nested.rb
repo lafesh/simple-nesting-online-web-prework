5b4fe34dd5d3a8fc9a08f9b7ae5935eecfc9a874
Gina Fedder <ginafedder@yahoo.com>
2018-09-19 03:10:41 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -84,7 +84,7 @@ def adding_matz
      }
      
      programmer_hash_new[:yukihiro_matsumoto] = {known_for: "Ruby", languages: ["LISP", "C"]}
-     programmer_hash.merge!
+     programmer_hash.merge!(programmer_hash_new)
 
     
 end
